c195e33bda5d896c7cedd5e05f8f255e6a8ad869
Aikol Tashbolot <aikoltashbolot@atasmbp01.gsm1900.org>
2019-08-12 15:00:13 -0700

    1-lesson: prop passing

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import ReactDom from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interface IFace{
+    color:string;
+}
+class App extends React.Component<IFace>{
+    render(){
+        return (
+            <h1>{this.props.color}</h1>
+        )
+    }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDom.render(<App color="red" />, document.querySelector('#root'));
