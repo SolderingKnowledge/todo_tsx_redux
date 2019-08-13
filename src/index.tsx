2c46d7aabb3f566edf0ea014b4c67d051ecec0b7
Aikol <imaikol@yahoo.com>
2019-08-13 11:50:17 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,51 @@
+// import React, { Fragment } from 'react';
+// import ReactDom from 'react-dom';
+
+// interface IProps{
+//     color:string;
+// }
+
+// class App extends React.Component<IProps>{
+//     state = {counter: 0}
+    
+//     increment=()=>{
+//         this.setState({
+//             counter: this.state.counter +1
+//         });
+//     }
+//     decrement=()=>{
+//         this.setState({
+//             counter: this.state.counter -1
+//         });
+//     } 
+//     render(){
+//         return (
+//             <Fragment>
+//                 <h1>{this.state.counter}</h1>
+//                 <button onClick={this.increment}> increment </button>
+//                 <button onClick={this.decrement}> decrement </button>
+//                 <h1>{this.props.color}</h1>
+//             </Fragment>
+//         )
+//     }
+// }
+
+// ReactDom.render(<App color="red" />, document.querySelector('#root'));
+
+
+
+
 import React, { Fragment } from 'react';
 import ReactDom from 'react-dom';
 
 interface IProps{
     color:string;
 }
+interface IState{
+    counter:number;
+}
 
-class App extends React.Component<IProps>{
+class App extends React.Component<IProps, IState>{
     state = {counter: 0}
     
     increment=()=>{
